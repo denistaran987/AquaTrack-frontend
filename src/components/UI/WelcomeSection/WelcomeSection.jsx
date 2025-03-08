384da35ce890c8f5f6d09faf539c8ddb90a87d52
Denis <denistaran482@gmail.com>
2025-03-08 23:58:49 +0200

    Add HomePage

--- src/components/UI/WelcomeSection/WelcomeSection.jsx
+++ src/components/UI/WelcomeSection/WelcomeSection.jsx
@@ -7,24 +7,22 @@ const WelcomeSection = () => {
   return (
     <section className={s.section}>
       <Logo />
-      <div className={s['title-container']}>
-        <p className={s.subtitle}>Record daily water intake and track</p>
-        <h1 className={s.title}>Water consumption tracker</h1>
-        <nav className={s.nav}>
-          <ul className={s.list}>
-            <li>
-              <Link className={clsx(s.link, s.first)} to="/signup">
-                Try tracker
-              </Link>
-            </li>
-            <li>
-              <Link className={clsx(s.link, s.second)} to="/signin">
-                Sign In
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <p className={s.subtitle}>Record daily water intake and track</p>
+      <h1 className={s.title}>Water consumption tracker</h1>
+      <nav className={s.nav}>
+        <ul className={s.list}>
+          <li>
+            <Link className={clsx(s.link, s.first)} to="/signup">
+              Try tracker
+            </Link>
+          </li>
+          <li>
+            <Link className={clsx(s.link, s.second)} to="/signin">
+              Sign In
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </section>
   );
 };
