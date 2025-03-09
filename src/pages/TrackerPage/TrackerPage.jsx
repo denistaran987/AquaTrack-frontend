aa9212ec62234bc928d30ebbf9ac720aad0f147e
sergeihodarkovski <sergeihodarkovski@gmail.com>
2025-03-09 13:43:35 +0100

    Save progress before merging main

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import s from './TrackerPage.module.css';
-import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
+import WaterMainInfo from '../../components/UI/WaterMainInfo/WaterMainInfo';
 
 const TrackerPage = () => {
   return (
