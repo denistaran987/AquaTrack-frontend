9ab6ad74a46eb50f8d501ee749922fb6d4459b26
Roman Hudz <hudzrom@gmail.com>
2025-03-10 20:47:10 +0200

    ProgressBar mobile

---
+++ src/components/UI/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './ProgressBar.module.css';
+
+const ProgressBar = ({ progress }) => {
+  return (
+    <div className={s.progressBar}>
+      <h2 className={s.today}>Today</h2>
+      <span
+        className={s.activePercent}
+        style={{ left: `calc(${progress}px * 1.74)` }}
+      >{`${progress}%`}</span>
+      <div className={s.progressBarContainer}>
+        <div className={s.progressBarFiller} style={{ width: `${progress}%` }}></div>
+        <div className={s.circle} style={{ left: `calc(${progress}% - 10px)` }}></div>
+      </div>
+      <div className={s.progressBarLabels}>
+        <span>0%</span>
+        <span>50%</span>
+        <span>100%</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+import ProgressBar from '../../components/ProgressBar/ProgressBar.jsx';
+
 const TrackerPage = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setProgress(65);
+  }, []);
+
   return (
     <>
       <h2>TrackerPage</h2>
+      <ProgressBar progress={progress} />
     </>
   );
 };
