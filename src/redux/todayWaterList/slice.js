20a25f7495dd2530c2080722d6ba98f1310d9f3a
besit06 <behemoth2054@gmail.com>
2025-03-12 20:47:38 +0100

    addEditModal

--- src/redux/todayWaterList/slice.js
+++ src/redux/todayWaterList/slice.js
@@ -7,9 +7,9 @@ const initialState = {
 export const slice = createSlice({
   name: 'todayWaterList',
   initialState,
- // extraReducers: builder => {
-  //  builder.addCase();
-  //},
+  extraReducers: builder => {
+    builder.addCase();
+  },
 });
 
 export default slice.reducer;
