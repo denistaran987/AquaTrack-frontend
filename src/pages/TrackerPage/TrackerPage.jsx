6502b14c44f0738dcd08c58202f77277026894da
Roman Hudz <hudzrom@gmail.com>
2025-03-12 19:35:09 +0200

    ProgressBar mobile (#19)
    * ProgressBar mobile
    
    * fixed
    
    ---------
    
    Co-authored-by: denistaran <denistaran482@gmail.com>

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from 'react';
+import ProgressBar from '../../components/UI/ProgressBar/ProgressBar.jsx';
 import React from 'react';
 import clsx from 'clsx';
 import s from './TrackerPage.module.css';
 import WaterMainInfo from '../../components/UI/WaterMainInfo/WaterMainInfo';
 
 const TrackerPage = () => {
-  return (
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setProgress(65);
+  }, []);
+
+  return ( 
     <div className="section">
       <div className={clsx('container', s['homepage-wrapper'])}>
         <WaterMainInfo />
+        <ProgressBar progress={progress} />
       </div>
     </div>
   );
