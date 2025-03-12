ba0dd99dd54818fcc2281a21318cf69a3ad5759b
Denis <denistaran482@gmail.com>
2025-03-12 18:32:00 +0200

    fix some bags

--- src/components/UI/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/UI/WaterMainInfo/WaterMainInfo.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
+import Logo from '../Logo/Logo';
 import s from './WaterMainInfo.module.css';
 import WaterDailyNorma from '../WaterDailyNorma/WaterDailyNorma';
 import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
 
 const WaterMainInfo = () => {
-  const handleAddWaterClick = () => {
-    console.log('Open WaterModal');
-  };
-
   return (
-    <section className={s.container}>
-      <h2 className={s.title}>AQUATRACK</h2>
+    <section className={s.section}>
+      <Logo />
       <div className={s.bottle}></div>
       <WaterDailyNorma />
-      <AddWaterBtn onClick={handleAddWaterClick} />
+      <AddWaterBtn />
     </section>
   );
 };
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import clsx from 'clsx';
 import s from './TrackerPage.module.css';
 import WaterMainInfo from '../../components/UI/WaterMainInfo/WaterMainInfo';
 
 const TrackerPage = () => {
   return (
-    <div className={s.container}>
-      <WaterMainInfo />
+    <div className="section">
+      <div className={clsx('container', s['homepage-wrapper'])}>
+        <WaterMainInfo />
+      </div>
     </div>
   );
 };
