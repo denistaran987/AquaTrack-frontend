30be8bce5018aa5378aa29a261a8c2745d4bddb7
Roman Hudz <hudzrom@gmail.com>
2025-03-13 18:54:45 +0200

    Dynamic resize of progressBar

--- src/components/UI/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/UI/WaterProgressBar/WaterProgressBar.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './WaterProgressBar.module.css';
 
 const WaterProgressBar = ({ progress }) => {
+  const getProgressBarWidthCoefficient = () => {
+    if (window.innerWidth >= 768) {
+      return 2.55;
+    } else {
+      return 1.74;
+    }
+  };
+
+  const [progressBarWidthCoefficient, setProgressBarWidthCoefficient] = useState(
+    getProgressBarWidthCoefficient()
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setProgressBarWidthCoefficient(getProgressBarWidthCoefficient());
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className={s.progressBar}>
       <h2 className={s.today}>Today</h2>
       <span
         className={s.activePercent}
         style={{
-          left: `calc(${progress}px * 1.72)`,
+          left: `calc(${progress}px * ${progressBarWidthCoefficient})`,
           color: progress === 100 && '#9be1a0',
         }}
       >{`${progress}%`}</span>
