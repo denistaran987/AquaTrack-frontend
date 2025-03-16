e707c5eb1f1127d4f8a9c26cebe4ab3201153112
Denis <denistaran482@gmail.com>
2025-03-16 13:00:53 +0200

    fix deploy Vercel

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -2,8 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { setAuthHeader } from '../auth/operations';
 
-axios.defaults.baseURL = 'https://aquatrack-backend-1b8z.onrender.com';
-
 export const deleteWaterEntry = createAsyncThunk(
   'waterList/deleteEntry',
   async (entryId, thunkAPI) => {
@@ -16,37 +14,33 @@ export const deleteWaterEntry = createAsyncThunk(
   }
 );
 
+export const getWaterByDay = createAsyncThunk('water/day', async ({ date, token }, thunkAPI) => {
+  if (!token) {
+    return thunkAPI.rejectWithValue('Unable to get current user');
+  }
 
-export const getWaterByDay = createAsyncThunk(
-  'water/day',
-  async ({date, token}, thunkAPI) => {
+  try {
+    setAuthHeader(token);
+    const response = await axios.get(`/water/day?date=${date}`);
+    return response.data.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response?.data || error.message);
+  }
+});
+
+export const getWaterByMonth = createAsyncThunk(
+  'water/month',
+  async ({ date, token }, thunkAPI) => {
     if (!token) {
       return thunkAPI.rejectWithValue('Unable to get current user');
     }
-  
+
     try {
       setAuthHeader(token);
-      const response = await axios.get(`/water/day?date=${date}`);
+      const response = await axios.get(`/water/month?date=${date}`);
       return response.data.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response?.data || error.message);
     }
-  });
-
-  export const getWaterByMonth = createAsyncThunk(
-    'water/month',
-    async ({date, token}, thunkAPI) => {
-      if (!token) {
-        return thunkAPI.rejectWithValue('Unable to get current user');
-      }
-    
-      try {
-        setAuthHeader(token);
-        const response = await axios.get(`/water/month?date=${date}`);
-        return response.data.data;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.response?.data || error.message);
-      }
-    });
-
-
+  }
+);
