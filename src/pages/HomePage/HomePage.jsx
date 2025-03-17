ef210b7733f2abb256e2650fba7f415a62d7a891
Denis <denistaran482@gmail.com>
2025-03-17 02:30:42 +0200

    Add logout button

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,15 @@ import clsx from 'clsx';
 import s from './HomePage.module.css';
 import WelcomeSection from '../../components/UI/WelcomeSection/WelcomeSection';
 import AdvantagesSection from '../../components/UI/AdvantagesSection/AdvantagesSection';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsOpenModal } from '../../redux/modal/selector';
+import Modal from '../../components/UI/Modal/Modal';
+import { toggleModal } from '../../redux/modal/slice';
+import LogOutModal from '../../components/UI/Modal/Components/LogOutModal/LogOutModal';
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const isOpen = useSelector(selectIsOpenModal);
   return (
     <>
       <section className="section">
@@ -11,6 +18,11 @@ const HomePage = () => {
           <WelcomeSection />
           <AdvantagesSection />
         </div>
+        {isOpen && (
+          <Modal isOpen={isOpen} toggleModal={() => dispatch(toggleModal())}>
+            {isOpen === 'Logout' && <LogOutModal />}
+          </Modal>
+        )}
       </section>
     </>
   );
