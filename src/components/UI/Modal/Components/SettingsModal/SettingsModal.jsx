1c967bce8560215c2e7a7e828647352aaa788abf
Denis <denistaran482@gmail.com>
2025-03-18 10:32:28 +0200

    fix Settings modal

--- src/components/UI/Modal/Components/SettingsModal/SettingsModal.jsx
+++ src/components/UI/Modal/Components/SettingsModal/SettingsModal.jsx
@@ -37,8 +37,6 @@ const SettingsModal = () => {
   const userDailyNorm = useSelector(selectUserDailyNorm);
   const isLoading = useSelector(selectUserIsLoading);
 
-  console.log(userGender);
-
   const [weight, setWeight] = useState(0);
   const [dailySportTime, setDailySportTime] = useState(0);
   const [dailyNorm, setDailyNorm] = useState(0);
@@ -187,175 +185,141 @@ const SettingsModal = () => {
                 <p className={css.title}>Your gender identity</p>
                 <div className={css.radioBtns}>
                   <label>
-                    <Field type="radio" name="gender" value="female" />
+                    <Field
+                      type="radio"
+                      name="gender"
+                      value="female"
+                      checked={gender === 'female'}
+                      onChange={handleGenderChange}
+                    />
                     <span className={css.customRadio}></span>
                     Woman
                   </label>
                   <label>
-                    <Field type="radio" name="gender" value="male" />
+                    <Field
+                      type="radio"
+                      name="gender"
+                      value="male"
+                      checked={gender === 'male'}
+                      onChange={handleGenderChange}
+                    />
                     <span className={css.customRadio}></span>
                     Man
                   </label>
                 </div>
               </div>
-              <div className={css.uploadWrapper}>
-                <label className={css.uploadLabel}>
-                  <svg className={css.uploadIcon}>
-                    <use href="/images/icons.svg#icon-upload"></use>
-                  </svg>
-                  Upload a photo
+              <div className={css.inputBlock}>
+                <div className={css.block}>
+                  <label htmlFor={nameId} className={css.title}>
+                    Your name
+                  </label>
                   <Field
-                    type="file"
-                    className={css.uploadBtn}
-                    onChange={handleAvatarUpload}
-                    accept="image/*"
-                    name="avatar"
+                    type="text"
+                    name="name"
+                    id={nameId}
+                    className={`${touched.name && errors.name ? css.errorInput : ''}`}
+                    onBlur={() => setFieldTouched('name', true)}
                   />
-                </label>
+                  <ErrorMessage className={css.errorMessage} name="name" component="div" />
+                </div>
+                <div className={css.block}>
+                  <label htmlFor={emailId} className={css.title}>
+                    Email
+                  </label>
+                  <Field
+                    type="text"
+                    name="email"
+                    id={emailId}
+                    className={`${touched.email && errors.email ? css.errorInput : ''}`}
+                    onBlur={() => setFieldTouched('email', true)}
+                  />
+                  <ErrorMessage className={css.errorMessage} name="email" component="div" />
+                </div>
               </div>
-            </div>
-            <div className={css.infoBlock}>
-              <div className={css.leftBlock}>
-                <div className={css.genderBlock}>
-                  <p className={css.title}>Your gender identity</p>
-                  <div className={css.radioBtns}>
-                    <label>
-                      <Field
-                        type="radio"
-                        name="gender"
-                        value="female"
-                        checked={gender === 'female'}
-                        onChange={handleGenderChange}
-                      />
-                      <span className={css.customRadio}></span>
-                      Woman
-                    </label>
-                    <label>
-                      <Field
-                        type="radio"
-                        name="gender"
-                        value="male"
-                        checked={gender === 'male'}
-                        onChange={handleGenderChange}
-                      />
-                      <span className={css.customRadio}></span>
-                      Man
-                    </label>
+              <div className={css.normaBlock}>
+                <p className={css.title}>My daily norma</p>
+                <div className={css.formulaBlock}>
+                  <div className={css.formulaWrapper}>
+                    <p>For woman:</p>
+                    <p className={css.formula}>V=(M*0,03) + (T*0,4)</p>
+                  </div>
+                  <div className={css.formulaWrapper}>
+                    <p>For man:</p>
+                    <p className={css.formula}>V=(M*0,04) + (T*0,6)</p>
                   </div>
                 </div>
-                <div className={css.inputBlock}>
-                  <div className={css.block}>
-                    <label htmlFor={nameId} className={css.title}>
-                      Your name
-                    </label>
-                    <Field
-                      type="text"
-                      name="name"
-                      id={nameId}
-                      className={`${touched.name && errors.name ? css.errorInput : ''}`}
-                      onBlur={() => setFieldTouched('name', true)}
-                    />
-                    <ErrorMessage className={css.errorMessage} name="name" component="div" />
-                  </div>
-                  <div className={css.block}>
-                    <label htmlFor={emailId} className={css.title}>
-                      Email
-                    </label>
-                    <Field
-                      type="text"
-                      name="email"
-                      id={emailId}
-                      className={`${touched.email && errors.email ? css.errorInput : ''}`}
-                      onBlur={() => setFieldTouched('email', true)}
-                    />
-                    <ErrorMessage className={css.errorMessage} name="email" component="div" />
-                  </div>
-                </div>
-                <div className={css.normaBlock}>
-                  <p className={css.title}>My daily norma</p>
-                  <div className={css.formulaBlock}>
-                    <div className={css.formulaWrapper}>
-                      <p>For woman:</p>
-                      <p className={css.formula}>V=(M*0,03) + (T*0,4)</p>
-                    </div>
-                    <div className={css.formulaWrapper}>
-                      <p>For man:</p>
-                      <p className={css.formula}>V=(M*0,04) + (T*0,6)</p>
-                    </div>
-                  </div>
-                  <div className={css.formulaInfo}>
-                    <p className={css.text}>
-                      <span className={css.asterisk}>*</span> V is the volume of the water norm in
-                      liters per day, M is your body weight, T is the time of active sports, or
-                      another type of activity commensurate in terms of loads (in the absence of
-                      these, you must set 0)
-                    </p>
-                  </div>
-                  <p className={css.mainText}>
-                    <svg className={css.icon}>
-                      <use href="./images/icons.svg#icon-exclamation-mark"></use>
-                    </svg>
-                    Active time in hours
+                <div className={css.formulaInfo}>
+                  <p className={css.text}>
+                    <span className={css.asterisk}>*</span> V is the volume of the water norm in
+                    liters per day, M is your body weight, T is the time of active sports, or
+                    another type of activity commensurate in terms of loads (in the absence of
+                    these, you must set 0)
                   </p>
                 </div>
+                <p className={css.mainText}>
+                  <svg className={css.icon}>
+                    <use href="./images/icons.svg#icon-exclamation-mark"></use>
+                  </svg>
+                  Active time in hours
+                </p>
               </div>
-              <div className={css.rightBlock}>
-                <div className={css.inputBlock}>
-                  <div className={css.block}>
-                    <label htmlFor={weightId}>Your weight in kilograms:</label>
-                    <Field
-                      type="number"
-                      name="weight"
-                      id={weightId}
-                      className={`${touched.weight && errors.weight ? css.errorInput : ''}`}
-                      onBlur={() => setFieldTouched('weight', true)}
-                      value={weight}
-                      onChange={handleChangeWeight}
-                    />
-                    <ErrorMessage className={css.errorMessage} name="weight" component="div" />
-                  </div>
-                  <div className={css.block}>
-                    <label htmlFor={timeId}>The time of active participation in sports:</label>
-                    <Field
-                      type="number"
-                      name="dailySportTime"
-                      id={timeId}
-                      className={`${touched.time && errors.time ? css.errorInput : ''}`}
-                      onBlur={() => setFieldTouched('dailySportTime', true)}
-                      value={dailySportTime}
-                      onChange={handleChangeDailySportTime}
-                    />
-                    <ErrorMessage
-                      className={css.errorMessage}
-                      name="dailySportTime"
-                      component="div"
-                    />
-                  </div>
+            </div>
+            <div className={css.rightBlock}>
+              <div className={css.inputBlock}>
+                <div className={css.block}>
+                  <label htmlFor={weightId}>Your weight in kilograms:</label>
+                  <Field
+                    type="number"
+                    name="weight"
+                    id={weightId}
+                    className={`${touched.weight && errors.weight ? css.errorInput : ''}`}
+                    onBlur={() => setFieldTouched('weight', true)}
+                    value={weight}
+                    onChange={handleChangeWeight}
+                  />
+                  <ErrorMessage className={css.errorMessage} name="weight" component="div" />
                 </div>
-                <div className={css.inputBlock}>
-                  <div className={css.resultBlock}>
-                    <p>
-                      The required amount of water in liters per day:{' '}
-                      <span className={css.result}>{`${dailyNorm.toFixed(1)}L`}</span>
-                    </p>
-                  </div>
-                  <div className={css.block}>
-                    <label htmlFor={waterIntakeId} className={css.title}>
-                      Write down how much water you will drink:
-                    </label>
-                    <Field type="number" name="dailyNorm" id={waterIntakeId} />
-                    <ErrorMessage className={css.errorMessage} name="dailyNorm" component="div" />
-                  </div>
+                <div className={css.block}>
+                  <label htmlFor={timeId}>The time of active participation in sports:</label>
+                  <Field
+                    type="number"
+                    name="dailySportTime"
+                    id={timeId}
+                    className={`${touched.time && errors.time ? css.errorInput : ''}`}
+                    onBlur={() => setFieldTouched('dailySportTime', true)}
+                    value={dailySportTime}
+                    onChange={handleChangeDailySportTime}
+                  />
+                  <ErrorMessage
+                    className={css.errorMessage}
+                    name="dailySportTime"
+                    component="div"
+                  />
+                </div>
+              </div>
+              <div className={css.inputBlock}>
+                <div className={css.resultBlock}>
+                  <p>
+                    The required amount of water in liters per day:{' '}
+                    <span className={css.result}>{`${dailyNorm.toFixed(1)}L`}</span>
+                  </p>
+                </div>
+                <div className={css.block}>
+                  <label htmlFor={waterIntakeId} className={css.title}>
+                    Write down how much water you will drink:
+                  </label>
+                  <Field type="number" name="dailyNorm" id={waterIntakeId} />
+                  <ErrorMessage className={css.errorMessage} name="dailyNorm" component="div" />
                 </div>
               </div>
             </div>
-            <button type="submit" className={css.saveBtn}>
-              Save
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+          </div>
+          <button type="submit" className={css.saveBtn}>
+            Save
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
